aa49f4cbc2267acaa6ab5708d35daf9ef6d382e4
gustavofernandes21 <113484424+gustavofernandes21@users.noreply.github.com>
2023-11-30 20:50:37 -0300

    Add files via upload

---
+++ Minimercado/src/Categoria.java
@@ -0,0 +1,17 @@
+
+class Categoria {
+    private String descCategoria;
+
+    public Categoria(String descCategoria) {
+        this.descCategoria = descCategoria;
+    }
+
+    // aplicação dos Getters e Setters
+    public String getDescCategoria() {
+        return descCategoria;
+    }
+
+    public void setDescCategoria(String descCategoria) {
+        this.descCategoria = descCategoria;
+    }
+}
